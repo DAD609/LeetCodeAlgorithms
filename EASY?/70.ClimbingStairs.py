9a07ef3ab265d09e0d4669b95550e8a8b351ee7d
ADam <67281187+DAD609@users.noreply.github.com>
2023-07-08 19:26:17 +0300

    Create 70.ClimbingStairs.py

---
+++ EASY?/70.ClimbingStairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climb(n):
+    #edge cases
+    if n<=2: return n
+    dp = [0]*(n+1) # considering zero steps we need n+1 places
+    dp[1]= 1
+    dp[2] = 2
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] +dp[i-2]
+    print(dp)
+    return dp[n]
