72c9216d849731d703eaca0df227ffa57a03cb9c
ADam <67281187+DAD609@users.noreply.github.com>
2023-07-08 19:29:19 +0300

    Create 1137.N-thTribonacciNumber.py

---
+++ EASY?/1137.N-thTribonacciNumber.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def tribonacci(self, n):
+        if n<=3: return n 
+        a=0
+        b=1
+        c=1
+        for i in n+1
+        a=b
+        b=c
+        c=d
+        d=a+b+c
+
+      return d
