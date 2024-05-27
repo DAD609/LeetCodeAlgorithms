be0da4a5b18c4990257c5e88f7911e5daa718e81
Almaz <67281187+DAD609@users.noreply.github.com>
2024-05-27 17:10:36 +0300

    Update 1227.Airplane Seat Assignment Probability.py

--- MEDIUM/1227.Airplane Seat Assignment Probability.py
+++ MEDIUM/1227.Airplane Seat Assignment Probability.py
@@ -1,5 +1,12 @@
 ##n passengers board an airplane with exactly n seats. The first passenger has lost the ticket and picks a seat randomly. But after that, the rest of the passengers will:
 
-Take their own seat if it is still available, and
-Pick other seats randomly when they find their seat occupied
-Return the probability that the nth person gets his own seat.##
+##Take their own seat if it is still available, and
+##Pick other seats randomly when they find their seat occupied
+##Return the probability that the nth person gets his own seat. 
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        if n == 1:
+            return 1.0
+        else:
+            return 0.5
+                
